d40772345abd23622a11061e4bd800c5fbf768a7
khushishri123 <shrivastavak057@gmail.com>
2023-08-24 20:11:59 +0530

    Nav change1

--- eg1.py
+++ eg1.py
@@ -1,6 +1,7 @@
 if __name__=="__main__":
     print("Changes done by feature1")
-    print("Hello Khushi, welcome to Switzerland")
+    print("Hello Khushi, Welcome to Switzerland and enjoy your new job")
+    print("Hello World")
     a=10
     b=20
     a,b=b,a
