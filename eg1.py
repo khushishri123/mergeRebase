ee6eef4d5f5873609156489999af8dc62046e23f
khushishri123 <shrivastavak057@gmail.com>
2023-08-24 23:59:07 +0530

    Feature3 change3

--- eg1.py
+++ eg1.py
@@ -5,7 +5,7 @@ if __name__=="__main__":
     a=10
     b=20
     a,b=b,a
-    print(a," ",b)
+    print(a," and ",b)
     print(a+b)
     print("feature3 branch, practising rebase")
     print("done")
